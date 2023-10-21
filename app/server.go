b4d06f6d085d87581a9503f591e11abfeef6b5e8
Srikant Rao <srikantrao@MacBook-Pro.local>
2023-10-21 16:35:22 -0700

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -17,9 +17,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
+	defer l.Close()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Fail to write response: ", err.Error())
 		os.Exit(1)
 	}
 }
